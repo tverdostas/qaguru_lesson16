62f94ddb7cf3f588e46093b9f7a5995335b053a1
Anastasiya Elkafrawi <atverdova78@gmail.com>
2025-10-02 22:48:19 +0400

    the test working with @WithLogin

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,27 +1,17 @@
 package steps;
 
 import io.qameta.allure.Step;
-import models.AddBookBody;
 import models.LoginBody;
 import models.UserLoginResponse;
 import models.UserResponse;
-import org.openqa.selenium.Cookie;
 
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.restassured.RestAssured.given;
 import static specs.SpecsBookStoreTest.*;
 import static tests.TestData.password;
 import static tests.TestData.username;
 
 public class LoginSteps {
-/*@Step("Добавить куки в запрос")
-public static void addCookies(UserLoginResponse userLoginResponse) {
-    open("/favicon.ico");
-    getWebDriver().manage().addCookie(new Cookie("userID", userLoginResponse.getUserId()));
-    getWebDriver().manage().addCookie(new Cookie("expires", userLoginResponse.getExpires()));
-    getWebDriver().manage().addCookie(new Cookie("token", userLoginResponse.getToken()));
-}*/
+
     @Step("Отправить запрос на логин в систему")
     public static UserLoginResponse login() {
         LoginBody authData = new LoginBody(username, password);
--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -1,8 +1,6 @@
 package tests;
 
 public class TestData {
-    public static final String username = "test1";
-            //System.getProperty ("username");
-    public static final String password = "QWE123%qwe";
-                    //System.getProperty ("password");
+    public static final String username = System.getProperty ("username");
+    public static final String password = System.getProperty ("password");
 }
